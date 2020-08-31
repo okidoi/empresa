76c790822c3fa718548637e097e5dbae40c71738
okidoi <okidoi@gmail.com>
2020-08-31 20:33:52 -0300

    Commit Inicial

---
+++ src/test/java/com/okidoi/curso/boot/EmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okidoi.curso.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
